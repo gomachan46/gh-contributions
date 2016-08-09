9f1205e9647c7bbfe2d3bd147936ab6b1438c27a
gomachan46 <shiro.gomachan46@gmail.com>
2016-08-09 23:51:38 +0900

    雑にgetした

--- cmd/github-streak/main.go
+++ cmd/github-streak/main.go
@@ -1,4 +1,15 @@
 package main
 
+import (
+	"net/http"
+	"fmt"
+)
+
 func main() {
+	resp, err := http.Get("https://github.com/users/gomachan46/contributions")
+	if err != nil {
+		fmt.Println("error", err)
+	}
+
+	fmt.Println(resp)
 }
