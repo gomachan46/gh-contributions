4691f7368527db86ae836e0eaaebd1dea7db1683
gomachan46 <shiro.gomachan46@gmail.com>
2016-08-10 23:30:52 +0900

    html文字列取ってきた

--- cmd/github-streak/main.go
+++ cmd/github-streak/main.go
@@ -3,13 +3,18 @@ package main
 import (
 	"net/http"
 	"fmt"
+	"io/ioutil"
 )
 
 func main() {
 	resp, err := http.Get("https://github.com/users/gomachan46/contributions")
 	if err != nil {
-		fmt.Println("error", err)
+		fmt.Println("http get error", err)
 	}
 
-	fmt.Println(resp)
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("body read error", err)
+	}
+	fmt.Println(string(body))
 }
