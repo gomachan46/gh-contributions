da31d2891dc4300e91f57530b2261014fe92b6fe
gomachan46 <shiro.gomachan46@gmail.com>
2016-08-13 03:49:53 +0900

    support multiple username

--- cmd/contributions/main.go
+++ cmd/contributions/main.go
@@ -13,11 +13,13 @@ func main() {
 		os.Exit(1)
 	}
 
-	c, err := contributions.Get(os.Args[1])
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "contribution: %v, err: %v", c, err)
-		os.Exit(1)
-	}
 	fmt.Fprint(os.Stdout, "username,start,end,total,currentStreak\n")
-	fmt.Fprintf(os.Stdout, "%s,%s,%s,%d,%d\n", c.Username, c.Start, c.End, c.Total, c.CurrentStreak)
+	for _, arg := range os.Args[1:] {
+		c, err := contributions.Get(arg)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "contribution: %v, err: %v", c, err)
+			os.Exit(1)
+		}
+		fmt.Fprintf(os.Stdout, "%s,%s,%s,%d,%d\n", c.Username, c.Start, c.End, c.Total, c.CurrentStreak)
+	}
 }
