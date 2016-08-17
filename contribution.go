91d9d1954513841c699f3bac404488b5b6fb8daf
gomachan46 <shiro.gomachan46@gmail.com>
2016-08-18 00:15:12 +0900

    add contrtibution struct and getter

--- contribution.go
+++ contribution.go
@@ -1,1 +1,34 @@
 package gh_contributions
+
+type Contribution struct {
+	username      string
+	from          string
+	to            string
+	total         int
+	currentStreak int
+	longestStreak int
+}
+
+func (c *Contribution) Username() string {
+	return c.username
+}
+
+func (c *Contribution) From() string {
+	return c.from
+}
+
+func (c *Contribution) To() string {
+	return c.to
+}
+
+func (c *Contribution) Total() int {
+	return c.total
+}
+
+func (c *Contribution) CurrentStreak() int {
+	return c.currentStreak
+}
+
+func (c *Contribution) LongestStreak() int {
+	return c.longestStreak
+}
