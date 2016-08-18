4cfd814bd97e32920b75170713bc880b47d209c7
gomachan46 <shiro.gomachan46@gmail.com>
2016-08-19 07:26:52 +0900

    remove contribution from gh-contributions

--- gh-contributions.go
+++ gh-contributions.go
@@ -2,47 +2,12 @@ package gh_contributions
 
 import (
 	"fmt"
-	"strconv"
-
 	"os"
-
+	"strconv"
 	"sync"
 
 	"github.com/PuerkitoBio/goquery"
 )
-
-type Contribution struct {
-	username      string
-	from          string
-	to            string
-	total         int
-	currentStreak int
-	longestStreak int
-}
-
-func (c *Contribution) Username() string {
-	return c.username
-}
-
-func (c *Contribution) From() string {
-	return c.from
-}
-
-func (c *Contribution) To() string {
-	return c.to
-}
-
-func (c *Contribution) Total() int {
-	return c.total
-}
-
-func (c *Contribution) CurrentStreak() int {
-	return c.currentStreak
-}
-
-func (c *Contribution) LongestStreak() int {
-	return c.longestStreak
-}
 
 func getFrom(doc *goquery.Document) string {
 	f, _ := doc.Find("rect").First().Attr("data-date")
