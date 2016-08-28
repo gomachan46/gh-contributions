c73e5d92ab8847b978535291619e484a9c605a71
gomachan46 <shiro.gomachan46@gmail.com>
2016-08-28 21:36:39 +0900

    Add getter's test

--- contribution_test.go
+++ contribution_test.go
@@ -17,3 +17,35 @@ func TestContribution_From(t *testing.T) {
 		t.Errorf("fail From() got: %s, want: %s", c.From(), expected)
 	}
 }
+
+func TestContribution_To(t *testing.T) {
+	expected := "2016-08-26"
+	c := &Contribution{to: expected}
+	if c.To() != expected {
+		t.Errorf("fail To() got: %s, want: %s", c.To(), expected)
+	}
+}
+
+func TestContribution_Total(t *testing.T) {
+	expected := 100
+	c := &Contribution{total: expected}
+	if c.Total() != expected {
+		t.Errorf("fail Total() got: %s, want: %s", c.Total(), expected)
+	}
+}
+
+func TestContribution_CurrentStreak(t *testing.T) {
+	expected := 10
+	c := &Contribution{currentStreak: expected}
+	if c.CurrentStreak() != expected {
+		t.Errorf("fail CurrentStreak() got: %s, want: %s", c.CurrentStreak(), expected)
+	}
+}
+
+func TestContribution_LongestStreak(t *testing.T) {
+	expected := 50
+	c := &Contribution{longestStreak: expected}
+	if c.LongestStreak() != expected {
+		t.Errorf("fail LongestStreak() got: %s, want: %s", c.LongestStreak(), expected)
+	}
+}
