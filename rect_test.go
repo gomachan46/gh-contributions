6c1004bfe830f3420d9d989bd9e2fbcb1538bc80
gomachan46 <shiro.gomachan46@gmail.com>
2016-09-07 09:35:17 +0900

    Add rect_test

--- rect_test.go
+++ rect_test.go
@@ -9,3 +9,19 @@ func TestRect_Date(t *testing.T) {
 		t.Errorf("fail Date() got: %s, want: %s", r.Date(), expected)
 	}
 }
+
+func TestRect_Count(t *testing.T) {
+	expected := "10"
+	r := &Rect{count: expected}
+	if r.Count() != expected {
+		t.Errorf("fail Count() got: %s, want: %s", r.Count(), expected)
+	}
+}
+
+func TestRect_Fill(t *testing.T) {
+	expected := "#111111"
+	r := &Rect{fill: expected}
+	if r.Fill() != expected {
+		t.Errorf("fail Fill() got: %s, want: %s", r.Fill(), expected)
+	}
+}
